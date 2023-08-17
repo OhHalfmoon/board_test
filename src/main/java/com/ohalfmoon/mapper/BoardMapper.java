5152a65b69d26ece46e63d7dfb8be43dd3348d5f
OhHalfmoon <sangoh7395@gmail.com>
2023-08-17 16:06:17 +0900

    영속영역 CRUD 구현. mapper를 통해 insert delete update 단일조회 구현

--- src/main/java/com/ohalfmoon/mapper/BoardMapper.java
+++ src/main/java/com/ohalfmoon/mapper/BoardMapper.java
@@ -7,6 +7,18 @@ import java.util.List;
 import com.ohalfmoon.domain.BoardVO;
 
 public interface BoardMapper {
-//	@Select("select * from tbl_board where bno > 0")
+//	@Select("select * from tbl_board where bno > 0") => Mapper.xml에서 대신 처리
 	public List<BoardVO> getList();
+	
+	public void insert(BoardVO board);
+	
+	public void insertSelectKey(BoardVO board);
+	
+	public BoardVO read(Long bno);
+	
+	public int delete(Long bno);
+	
+	public int update(BoardVO board);
+	
+	
 }
--- src/test/java/com/ohalfmoon/mapper/BoardMapperTests.java
+++ src/test/java/com/ohalfmoon/mapper/BoardMapperTests.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.ohalfmoon.domain.BoardVO;
+
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
@@ -20,6 +22,55 @@ public class BoardMapperTests {
 	@Test
 	public void testGetList() {
 		mapper.getList().forEach(board -> log.info(board));
+	}
+	
+	@Test
+	public void testInsert() {
+		BoardVO board = new BoardVO();
+		board.setTitle("테스트 코드 작성글");
+		board.setContent("테스트 코드 작성 내용");
+		board.setWriter("테스트");
+		
+		mapper.insert(board);
+		
+		log.info(board);
+	}
+	
+	@Test
+	public void testInsertSelectKey() {
+		BoardVO vo = new BoardVO();
+		vo.setTitle("테스트확인");
+		vo.setContent("확인");
+		vo.setWriter("테스트");
+		
+		mapper.insertSelectKey(vo);
+		
+		log.info(vo);
+	}
+	
+	@Test
+	public void testRead() {
+		//존재하는 게시물 번호로 테스트
+		BoardVO boardVO = mapper.read(55L);
+		log.info(boardVO);
+	}
+	
+	@Test
+	public void testDelete() {
+		log.info("DELETE COUNT: " + mapper.delete(42L));
+	}
+	
+	@Test
+	public void testUpdate() {
+		BoardVO vo = new BoardVO();
+		
+		vo.setBno(40L);
+		vo.setTitle("업데이트테스트");
+		vo.setContent("글내용수정");
+		vo.setWriter("새로운작성자");
+		
+		int count = mapper.update(vo);
+		log.info("UPDATE COUNT: " + count);
 	}
 	
 
