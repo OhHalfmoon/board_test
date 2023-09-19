196cd3a50097c1775d81b772a1e79f797bd7cbe6
OhHalfmoon <sangoh7395@gmail.com>
2023-09-19 16:38:19 +0900

    " 1.파일 크기 및 확장자 제한 : 실습을 위해 5MB이상 및 exe,sh, zip, alz파일 업로드 제한 2. 파일 업로드시 년/월/일 폴더 생성 3.파일명 중복방지를 위한 UUID 적용 : 랜덤uuid값과 기존 파일명 사이에 "_"를 통해 구분

--- src/main/java/com/ohalfmoon/controller/UploadExController.java
+++ src/main/java/com/ohalfmoon/controller/UploadExController.java
@@ -1,6 +1,9 @@
 package com.ohalfmoon.controller;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,7 +15,15 @@ import lombok.extern.log4j.Log4j;
 
 @Controller
 @Log4j
-public class UploadController {
+public class UploadExController {
+	
+	private String getFolder() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = new Date();
+		String str = sdf.format(date);
+		return str.replace("-", File.separator);
+	}
+	
 	@GetMapping("/uploadForm")
 	public void uploadForm() {
 		log.info("upload form");
@@ -42,10 +53,20 @@ public class UploadController {
 	public void uploadAjax() {
 		log.info("upload ajax");
 	}
+	
 	@PostMapping("/uploadAjaxAction")
 	public void uploadAjaxPost(MultipartFile[] uploadFile) {
 		log.info("update ajax post......");
 		String uploadFolder = "C:\\upload";
+		
+		// make folder
+		File uploadPath = new File(uploadFolder, getFolder());
+		log.info("upload path: " + uploadPath);
+		if (uploadPath.exists() == false) {
+			uploadPath.mkdirs();
+		}
+		//make yyyy/MM/dd folder
+		
 		for (MultipartFile multipartFile : uploadFile) {
 			log.info("-------------------------------------");
 			log.info("Upload File name: " + multipartFile.getOriginalFilename());
@@ -55,7 +76,12 @@ public class UploadController {
 			//IE has file path
 			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\")+1);
 			log.info("only file name: " + uploadFileName);
-			File saveFile = new File(uploadFolder, uploadFileName);
+			
+			UUID uuid = UUID.randomUUID();
+			uploadFileName = uuid.toString() + "_" + uploadFileName;
+			
+			// File saveFile = new File(uploadFolder, uploadFileName);
+			File saveFile = new File(uploadPath, uploadFileName);
 			try {
 				multipartFile.transferTo(saveFile);
 			} catch (Exception e) {
@@ -64,4 +90,7 @@ public class UploadController {
 			}
 		}
 	}
+	
+
+	
 }
