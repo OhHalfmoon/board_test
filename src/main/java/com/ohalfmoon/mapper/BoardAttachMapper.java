3e6659ff2bc4da948d24b0bfd18fd0782ed37396
OhHalfmoon <sangoh7395@gmail.com>
2023-10-30 16:03:26 +0900

    잘못 업로드된 파일 삭제 1) 첨부파일만 등록하고 게시물 등록을 취소했을때 2) 게시물 수정시 파일을 삭제했으나 실제 폴더에서 기존 파일은 삭제되지 않았을 때
    ->사용자가 Ajax로 작업을 한 후 비정상적으로 브라우저를 종료하거나 페이지를 나갈경우 생기는 문제 대처

--- src/main/java/com/ohalfmoon/mapper/BoardAttachMapper.java
+++ src/main/java/com/ohalfmoon/mapper/BoardAttachMapper.java
@@ -11,5 +11,8 @@ public interface BoardAttachMapper {
 	public List<BoardAttachVO> findByBno(Long bno);
 	public void deleteAll(Long bno);
 	
+	//전날에 등록된 모든 파일의 목록 확인
+	public List<BoardAttachVO> getOldFiles();
+	
 
 }
